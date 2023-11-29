9125c2ada8ea0b34067971b80af823b7f52a86e1
Antonio <76496876+acavlovic@users.noreply.github.com>
2023-11-29 12:12:25 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,113 @@
+const passwordOne = document.getElementById("password1-el");
+const passwordTwo = document.getElementById("password2-el");
+document
+  .querySelector("#generate-el")
+  .addEventListener("click", generatePasswords);
+
+const characters = [
+  "A",
+  "B",
+  "C",
+  "D",
+  "E",
+  "F",
+  "G",
+  "H",
+  "I",
+  "J",
+  "K",
+  "L",
+  "M",
+  "N",
+  "O",
+  "P",
+  "Q",
+  "R",
+  "S",
+  "T",
+  "U",
+  "V",
+  "W",
+  "X",
+  "Y",
+  "Z",
+  "a",
+  "b",
+  "c",
+  "d",
+  "e",
+  "f",
+  "g",
+  "h",
+  "i",
+  "j",
+  "k",
+  "l",
+  "m",
+  "n",
+  "o",
+  "p",
+  "q",
+  "r",
+  "s",
+  "t",
+  "u",
+  "v",
+  "w",
+  "x",
+  "y",
+  "z",
+  "0",
+  "1",
+  "2",
+  "3",
+  "4",
+  "5",
+  "6",
+  "7",
+  "8",
+  "9",
+  "~",
+  "`",
+  "!",
+  "@",
+  "#",
+  "$",
+  "%",
+  "^",
+  "&",
+  "*",
+  "(",
+  ")",
+  "_",
+  "-",
+  "+",
+  "=",
+  "{",
+  "[",
+  "}",
+  "]",
+  ",",
+  "|",
+  ":",
+  ";",
+  "<",
+  ">",
+  ".",
+  "?",
+  "/",
+];
+
+function generatePassword(passwordLength) {
+  let password = "";
+  for (let i = 0; i < passwordLength; i++) {
+    const index = Math.floor(characters.length * Math.random());
+    password += characters[index];
+  }
+  return password;
+}
+
+function generatePasswords() {
+  passwordOne.textContent = generatePassword(12);
+  passwordTwo.textContent = generatePassword(12);
+}
